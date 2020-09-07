9dce3086b4fe23e0c9118c5e829d1c1d663c171e
Krzysztof Pniak <krzysztof.pniak@gmail.com>
2020-09-07 08:55:29 +0200

    style(vue-widget): renames folder

---
+++ packages/vue-widget/rollup.config.js
@@ -0,0 +1,21 @@
+import vue from 'rollup-plugin-vue';
+import commonjs from 'rollup-plugin-commonjs';
+import buble from '@rollup/plugin-buble'; // Transpile/polyfill with reasonable browser support
+
+export default {
+    input: 'src/index.js', // Path relative to package.json
+    output: {
+        file: 'dist/bundle.js',
+        format: 'cjs'
+    },
+    plugins: [
+        commonjs(),
+        vue({
+            css: true, // Dynamically inject css as a <style> tag
+            compileTemplate: true, // Explicitly convert template to render function
+        }),
+        buble(), // Transpile to ES5
+    ],
+};
+
+
